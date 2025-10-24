35d09f6f1335ff630578c9836761e8eed78a24ee
Albert Boddema <albert@leomusic.nl>
2025-10-24 19:56:08 +0200

    Header aangepast voor live en locaal

--- includes/header.php
+++ includes/header.php
@@ -2,13 +2,28 @@
 <html>
 <head>
     <title>VillaKlant</title>
-    <link rel="stylesheet" href="/VillaKlant/css/style.css">
+    <?php
+    // Eenvoudige en betrouwbare path detectie
+    $current_file = $_SERVER['SCRIPT_NAME'];
+    $base_path = '';
+    
+    // Als we in een subdirectory zijn (bevat / na VillaKlant of niet in root)
+    if (strpos($current_file, '/klanten/') !== false || 
+        strpos($current_file, '/bedrijf/') !== false || 
+        strpos($current_file, '/relatie/') !== false) {
+        $base_path = '../';
+    }
+    
+    // Debug info (kan later weggehaald worden)
+    // echo "<!-- Current file: $current_file, Base path: '$base_path' -->";
+    ?>
+    <link rel="stylesheet" href="<?php echo $base_path; ?>CSS/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/VillaKlant/index.php">
-            <img src="/VillaKlant/css/Villa-ProCtrl-Logo.svg" alt="Villa-ProCtrl Logo" class="logo">
+        <a class="navbar-brand" href="<?php echo $base_path; ?>index.php">
+            <img src="<?php echo $base_path; ?>CSS/Villa-ProCtrl-Logo.svg" alt="Villa-ProCtrl Logo" class="logo">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -16,25 +31,25 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="/VillaKlant/klanten/create_klant.php">Klant toevoegen</a>
+                    <a class="nav-link" href="<?php echo $base_path; ?>klanten/create_klant.php">Klant toevoegen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/VillaKlant/klanten/read_klant.php">Klanten bekijken</a>
+                    <a class="nav-link" href="<?php echo $base_path; ?>klanten/read_klant.php">Klanten bekijken</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/VillaKlant/klanten/search_klant.php">Klant zoeken</a>
+                    <a class="nav-link" href="<?php echo $base_path; ?>klanten/search_klant.php">Klant zoeken</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/VillaKlant/bedrijf/create_bedrijf.php">Bedrijf toevoegen</a>
+                    <a class="nav-link" href="<?php echo $base_path; ?>bedrijf/create_bedrijf.php">Bedrijf toevoegen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/VillaKlant/bedrijf/read_bedrijf.php">Bedrijven bekijken</a>
+                    <a class="nav-link" href="<?php echo $base_path; ?>bedrijf/read_bedrijf.php">Bedrijven bekijken</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/VillaKlant/bedrijf/search_bedrijf.php">Bedrijf zoeken</a>
+                    <a class="nav-link" href="<?php echo $base_path; ?>bedrijf/search_bedrijf.php">Bedrijf zoeken</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/VillaKlant/relatie/index.php">Relatie Zoeken</a>
+                    <a class="nav-link" href="<?php echo $base_path; ?>relatie/index.php">Relatie Zoeken</a>
                 </li>
             </ul>
         </div>
